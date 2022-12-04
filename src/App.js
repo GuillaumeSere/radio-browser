2e4977245db8cbfa86d679481d724e7a7799e08d
Guillaume Sere <guillaumesere@hotmail.fr>
2022-12-04 16:23:23 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Radio from './components/Radio';
+import guirlande1 from "./light.png"
+import guirlande2 from "./light1.png"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="banner">
+    <img className="guirlande" src={guirlande1} alt="" />
+    <img className="guirlande1" src={guirlande2} alt="" />
+    <img className="guirlande2" src={guirlande2} alt="" />
+    <img className="guirlande3" src={guirlande2} alt="" />
+    <img className="guirlande4" src={guirlande2} alt="" />
+    <img className="guirlande5" src={guirlande2} alt="" />
+    <h1>Live Radio Player</h1>
+    <h2>Choisissez votre station préférée</h2>
+    <img className="guirlande6" src={guirlande1} alt="" />
+    <img className="guirlande7" src={guirlande2} alt="" />
+    <img className="guirlande8" src={guirlande2} alt="" />
+    <img className="guirlande9" src={guirlande2} alt="" />
+    <img className="guirlande10" src={guirlande2} alt="" />
+    <img className="guirlande11" src={guirlande2} alt="" />
     </div>
+    <Radio />
+</div>
   );
 }
 
---
+++ src/components/Radio.jsx
@@ -0,0 +1,100 @@
+import React, { useEffect, useState } from 'react'
+import defaultImage from "../radio.jpg";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
+const RadioBrowser = require('radio-browser')
+
+const Radio = () => {
+    const [stations, setStations] = useState();
+    const [stationFilter, setStationFilter] = useState("all");
+
+    useEffect(() => {
+        setupApi(stationFilter).then((data) => {
+            console.log(data);
+            setStations(data);
+        });
+    }, [stationFilter]);
+
+    const setupApi = async (stationFilter) => {
+        const api = RadioBrowser
+
+        const stations = await api
+            .searchStations({
+                language: "english",
+                tag: stationFilter,
+                limit: 40,
+            })
+            .then((data) => {
+                return data;
+            });
+
+        return stations;
+    };
+
+    const filters = [
+        "all",
+        "classical",
+        "country",
+        "dance",
+        "disco",
+        "house",
+        "jazz",
+        "pop",
+        "rap",
+        "retro",
+        "rock",
+    ];
+
+    const setDefaultSrc = (event) => {
+        event.target.src = defaultImage;
+    };
+
+    return (
+        <div className="radio">
+            <div className="filters">
+                {filters.map((filter, index) => (
+                    <span
+                        key={index}
+                        className={stationFilter === filter ? "selected" : ""}
+                        onClick={() => setStationFilter(filter)}
+                    >
+                        {filter}
+                    </span>
+                ))}
+            </div>
+            <div className="stations">
+                {stations &&
+                    stations.map((station, index) => {
+                        return (
+                            <div className="station" key={index}>
+                                <div className="stationName">
+                                    <img
+                                        className="logo"
+                                        src={station.favicon}
+                                        alt="station logo"
+                                        onError={setDefaultSrc}
+                                    />
+                                    <div className="name">{station.name}</div>
+                                </div>
+
+                                <AudioPlayer
+                                    className="player"
+                                    src={station.url_resolved}
+                                    showJumpControls={false}
+                                    layout="stacked"
+                                    customProgressBarSection={[]}
+                                    customControlsSection={["MAIN_CONTROLS", "VOLUME_CONTROLS"]}
+                                    autoPlayAfterSrcChange={false}
+                                />
+                            </div>
+                        );
+                    })}
+            </div>
+            <div className="footer">
+                <p> Copyrights 2022, Developed by <span>Guillaume SERE </span>with React</p>
+            </div>
+        </div>
+    )
+}
+
+export default Radio
